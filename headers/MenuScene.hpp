7a8998bd050764c4887db96af93273eeb66ed70f
Ramiro <ramiro.sgaray@gmail.com>
2018-03-29 12:14:22 -0300

    Se anadieron creditos a la escena de Menu

--- headers/MenuScene.hpp
+++ headers/MenuScene.hpp
@@ -7,12 +7,13 @@ class MenuScene : public BaseScene {
 private:
 	sf::Text title_text;
 	sf::Text transition_text;
+  sf::Text credits_text;
 	sf::Font font;
 	sf::View view;
 	sf::Music devilman;
 	sf::Texture portal;
 	sf::Sprite spr_portal;
-	
+
 public:
 	MenuScene();
 	void update(float elapsed, sf::Event e) override;
--- src/MenuScene.cpp
+++ src/MenuScene.cpp
@@ -7,28 +7,36 @@ MenuScene::MenuScene() {
 	view.reset(sf::FloatRect(0, 0, 1920, 1080));
 	view.setCenter(1920 / 2, 1080 / 2);
 	view.zoom(1);
-	
+
 	font.loadFromFile("fonts/munro.ttf");
 	title_text.setFont(font);
-	title_text.setString("hexcape 0.2");
+	title_text.setString("Hexcape 1.0");
 	title_text.setCharacterSize(50);
 	title_text.setOrigin(title_text.getLocalBounds().width / 2.0,
 							 title_text.getLocalBounds().height / 2.0);
 	title_text.setPosition(1920 / 2, 1080 / 2);
-	
-	
+
+
 	transition_text.setFont(font);
 	transition_text.setString("Presiona <Enter> para jugar");
 	transition_text.setCharacterSize(24);
 	transition_text.setPosition(1920.0 / 2 - transition_text.getGlobalBounds().width / 2.0,
 								   1080.0 * 3.0 / 4.0 - transition_text.getGlobalBounds().height / 2.0);
-	
+
+  credits_text.setFont(font);
+  credits_text.setString("Copyright 2018\nIgnacio Fucksmann\nRamiro Garay");
+  credits_text.setCharacterSize(18);
+  sf::Vector2f tam_credits(credits_text.getLocalBounds().width,
+                           credits_text.getLocalBounds().height);
+  credits_text.setOrigin(tam_credits.x, tam_credits.y);
+  credits_text.setPosition(1920-5, 1080-5);
+
 	portal.loadFromFile("assets/portal.png");
 	spr_portal.setTexture(portal);
 	spr_portal.setOrigin(45, 45);
 	spr_portal.setPosition(1920/2,1080/2);
 	spr_portal.setScale(20,20);
-	
+
 	if (!devilman.openFromFile("sounds/devilman-no-uta.ogg")){
 	std::cout<<"no abrio devilmannouta"<<std::endl;
 	}
@@ -52,4 +60,5 @@ void MenuScene::draw(sf::RenderWindow &w) {
 	w.draw(spr_portal);
 	w.draw(title_text);
 	w.draw(transition_text);
+  w.draw(credits_text);
 }
